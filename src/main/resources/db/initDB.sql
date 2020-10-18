b67ad469d2f41b980eb7b41dc3b87876389e86d4
Fox2k <fox2k@protonmail.com>
2020-10-18 19:45:38 +0300

    HW3_without_Optional

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
+
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
@@ -14,7 +16,21 @@ CREATE TABLE users
   enabled          BOOL DEFAULT TRUE       NOT NULL,
   calories_per_day INTEGER DEFAULT 2000    NOT NULL
 );
+
+CREATE TABLE meals
+(
+    id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    date_time        TIMESTAMP               NOT NULL,
+    description      VARCHAR                 NOT NULL,
+    calories         VARCHAR                 NOT NULL,
+    user_id          INTEGER                 NOT NULL,
+    CONSTRAINT meals_idx UNIQUE (user_id, date_time),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+
+CREATE UNIQUE INDEX meals_unique_date_time_idx ON meals (date_time);
 
 CREATE TABLE user_roles
 (
